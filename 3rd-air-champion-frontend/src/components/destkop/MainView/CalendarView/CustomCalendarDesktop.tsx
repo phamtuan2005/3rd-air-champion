9d3a97a05ef3eb5d24e18c942b6fe497f86f3913
phamtuan2005 <phamtuan2005@users.noreply.github.com>
2026-04-20 20:16:12 -0700

    fix: make last week of the month visible and tappable on mobile phone

--- 3rd-air-champion-frontend/src/components/destkop/MainView/CalendarView/CustomCalendarDesktop.tsx
+++ 3rd-air-champion-frontend/src/components/destkop/MainView/CalendarView/CustomCalendarDesktop.tsx
@@ -583,7 +583,7 @@ const CustomCalendar = ({
   return (
     <div
       ref={scrollContainerRef}
-      className="h-full overflow-y-scroll snap-y snap-mandatory"
+      className="flex-1 min-h-0 overflow-y-scroll snap-y snap-mandatory"
       onScroll={handleScroll}
     >
       {months.map((month, index) => {
