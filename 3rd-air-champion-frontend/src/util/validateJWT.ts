4de513467f18d11c4b7b4e38eb5bad5a3909490a
Anh Tri Pham <anhtp5@uci.edu>
2026-04-25 20:29:59 -0500

    fix: minor private route

--- 3rd-air-champion-frontend/src/util/validateJWT.ts
+++ 3rd-air-champion-frontend/src/util/validateJWT.ts
@@ -1,10 +1,15 @@
 import { jwtDecode, JwtPayload } from "jwt-decode";
 
+interface CustomJwtPayload extends JwtPayload {
+  role?: string;
+}
+
 export const isTokenValid = (): boolean => {
   const token = localStorage.getItem("token");
   if (!token) return false;
 
-  const decoded = jwtDecode<JwtPayload>(token);
+  const decoded = jwtDecode<CustomJwtPayload>(token);
+  if (decoded.role && decoded.role === "TiBook") return false;
   const currentTime = Date.now() / 1000;
   return decoded.exp ? decoded.exp > currentTime : false;
 };
